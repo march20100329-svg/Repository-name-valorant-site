bf9931742aa35aed79b1fc00ce57ca29b24ff911
march20100329-svg <march20100329@gmail.com>
2026-03-29 02:01:37 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+
 app.use(cors());
 app.use(express.json());
 
-// 測試用首頁
+// ⭐ 強制絕對路徑
+const publicPath = path.join(__dirname, "public");
+console.log("PUBLIC PATH:", publicPath);
+
+app.use(express.static(publicPath));
+
+// 測試 root
 app.get("/", (req, res) => {
-    res.send("後端正常運行 ✔");
+    res.sendFile(path.join(publicPath, "index.html"));
 });
 
 // 接收登入資料
@@ -26,7 +32,8 @@ app.post("/login", (req, res) => {
         message: "後端已收到資料"
     });
 });
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(PORT, () => {
+    console.log("Server running on " + PORT);
 });
